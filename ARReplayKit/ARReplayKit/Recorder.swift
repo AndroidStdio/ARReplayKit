0601c9265fe5d9828612ccc1d40a682f7e170d74
mac126 <tulaomao152@126.com>
2019-01-15 16:05:32 +0800

    删除没用文件

--- ARReplayKit/ARReplayKit/Recorder.swift
+++ ARReplayKit/ARReplayKit/Recorder.swift
@@ -102,47 +102,14 @@ class Recorder: NSObject {
                    print("--remove file error-\(error.localizedDescription)")
                 }
             }
-            assetWriter = try! AVAssetWriter(outputURL: fileURL, fileType:
-                AVFileType.mp4)
-            let videoOutputSettings: Dictionary<String, Any> = [
-                AVVideoCodecKey : AVVideoCodecType.h264,
-                AVVideoWidthKey : UIScreen.main.bounds.size.width,
-                AVVideoHeightKey : UIScreen.main.bounds.size.height
-            ];
-            
-            videoInput  = AVAssetWriterInput (mediaType: AVMediaType.video, outputSettings: videoOutputSettings)
-            videoInput.expectsMediaDataInRealTime = true
-            assetWriter.add(videoInput)
             
             RPScreenRecorder.shared().startCapture(handler: { (sample, bufferType, error) in
-                //                print(sample,bufferType,error)
                 
                 recordingHandler(error)
-                
-                if CMSampleBufferDataIsReady(sample)
-                {
-                    if self.assetWriter.status == AVAssetWriter.Status.unknown
-                    {
-                        self.assetWriter.startWriting()
-                        self.assetWriter.startSession(atSourceTime: CMSampleBufferGetPresentationTimeStamp(sample))
-                    }
-                    
-                    if self.assetWriter.status == AVAssetWriter.Status.failed {
-                        print("Error occured, status = \(self.assetWriter.status.rawValue), \(self.assetWriter.error!.localizedDescription) \(String(describing: self.assetWriter.error))")
-                        return
-                    }
-                    
-                    if (bufferType == .video)
-                    {
-                        if self.videoInput.isReadyForMoreMediaData
-                        {
-                            self.videoInput.append(sample)
-                        }
-                    }
+                if bufferType == .video {
                 }
                 
                 recordingHandler(error)
-                //                debugPrint(error)
             })
         } else {
             // Fallback on earlier versions
@@ -152,10 +119,7 @@ class Recorder: NSObject {
     func stopRecording(handler: @escaping (Error?) -> Void) {
         if #available(iOS 11.0, *) {
             RPScreenRecorder.shared().stopCapture { (error) in
-                    handler(error)
-                    self.assetWriter.finishWriting {
-                            print(ReplayFileUtil.fetchAllReplays())
-                    }
+                handler(error)
             }
         } else {
             // Fallback on earlier versions
